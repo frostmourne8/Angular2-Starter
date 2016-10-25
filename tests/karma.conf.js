eece0c25ff56649fb2e4604a9f0b7be826ed4b44
Josh Wakefield <JoshWakefield45@gmail.com>
2016-10-25 10:46:05 -0400

    Laying some of the foundation

---
+++ tests/karma.conf.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var webpackConfig = require('webpack/webpack.test');
+
+module.exports = (config) => {
+    config.set({
+        
+        browsers: ['Chrome', 'PhantomJS'],
+        frameworks: ['jasmine'],
+
+        phantomJsLauncher: {
+            exitOnResourceError: true
+        },
+
+        files: [
+            {pattern: 'karma-entry.js', watched: false}
+        ],
+
+        preprocessors: {
+            'karma-entry.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackConfig,
+
+        webpackMiddleware: { stats: 'errors-only' },
+
+        webpackServer: { noInfo: true },
+
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false
+    });
+}
